553d419a84aa1f3808c8aa41e0ebdca38f97d93a
Maham-Afzaal <mahamafzaal96@gmail.com>
2024-06-08 21:28:34 +0100

    updated menu

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,42 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Link from 'next/link'
-import main from '/public/main.png';
+import Image from "next/image";
+import styles from "./page.module.css";
+import Link from "next/link";
+import main from "/public/main.png";
 export default function Home() {
   return (
     <main>
-  <head>
-            <title>Home</title>
-            <meta name="description" content="Your page description here" />
-            <link rel="icon" href="/home.png" />
-          </head>
-    <div className={styles.container}>
-      <div className={styles.item}>
-        <h1 className={styles.title}>
-          Better design for your digital products.
-        </h1>
-        <p className={styles.desc}>
-          Turning your Idea into Reality. We bring together the teams from the
-          global tech industry.
-        </p>
-        <Link href='#'><button style={{padding:'8px',color:'white',backgroundColor:'#2a9d8f',fontWeight:'bold',fontSize:'1rem'}}>See our works</button></Link>
+      <head>
+        <title>Home</title>
+        <meta name="description" content="Your page description here" />
+        <link rel="icon" href="/home.png" />
+      </head>
+      <div className={styles.container}>
+        <div className={styles.item}>
+          <h1 className={styles.title}>
+            Better design for your digital products.
+          </h1>
+          <p className={styles.desc}>
+            Turning your Idea into Reality. We bring together the teams from the
+            global tech industry.
+          </p>
+          <Link href="#">
+            <button
+              style={{
+                padding: "8px",
+                color: "white",
+                backgroundColor: "#2a9d8f",
+                fontWeight: "bold",
+                fontSize: "1rem",
+              }}
+            >
+              See our works
+            </button>
+          </Link>
+        </div>
+        <div className={styles.item}>
+          <Image src={main} alt="" className={styles.img} />
+        </div>
       </div>
-      <div className={styles.item}>
-        <Image src={main} alt="" className={styles.img}/>
-      </div>
-    </div>
     </main>
-  )
+  );
 }
